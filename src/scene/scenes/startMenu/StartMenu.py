9d4b3ae69944b59f4d1600d5ae881d415cffeeb1
daiwua <731130883@qq.com>
2022-04-30 16:17:52 +0800

    StarMenue

--- src/scene/scenes/startMenu/StartMenu.py
+++ src/scene/scenes/startMenu/StartMenu.py
@@ -6,12 +6,14 @@ from src.scene.scene import Scene
 class startMenu(Scene):
     def display(self):
         #调用资源管理器中的背景图片
-        Background=self.KOF.mediaLibrary.PicLibrary['KOF_StarBG2'].pygamePic()
-        Gobutton= self.mediaLibrary.PicLibrary['GO1'].pygamePic()
-        self.config.bgmService.play("BGM1")
+        Background=self.KOF.mediaLibrary.PicLibrary['KOF_StarBG1'].pygamePic()
+        Gobutton= self.KOF.mediaLibrary.PicLibrary['GO2'].pygamePic()
+        Gif1=self.KOF.mediaLibrary.PicLibrary['Ashkofxii'].pygamePic()
+        self.KOF.bgmService.play("BGM1")
         #打印在屏幕上
-        self.screen.blit(Background, (0, 0))
-        self.screen.blit(Gobutton, (30,30))
+        self.KOF.screen.blit(Background, (0, 0))
+        self.KOF.screen.blit(Gobutton, (100,50))
+        self.KOF.screen.blit(Gif1,(300,100))
         pass
 
     def clear(self):

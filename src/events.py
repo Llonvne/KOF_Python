bc99b9ecdde2885fd1772c8a8ccfeedb25bec96f
Llonvne <work@llonvne.cn>
2022-04-30 13:01:53 +0800

    更新 event 起始点

--- src/events.py
+++ src/events.py
@@ -1,5 +1,6 @@
 import pygame.constants
 
+DIFF = 10000
 MUSIC_END = pygame.constants.USEREVENT + 1
 
 """
@@ -7,22 +8,27 @@ MUSIC_END = pygame.constants.USEREVENT + 1
 ST_xxxx
 """
 ST_events = dict()
+ST_EVENTS_START = pygame.constants.USEREVENT + 1 + DIFF
 ST_events["ST_StartGame"] = pygame.constants.USEREVENT + 2
 
 """
 选角色
 CH_
 """
+CH_EVENTS_START = pygame.constants.USEREVENT + 1 + 2 * DIFF
 CH_events = dict()
 
 
 """
 BT_
 """
+BT_EVENTS_START = pygame.constants.USEREVENT + 1 + 3 * DIFF
 BT_events = dict()
 
 """
 结束菜单
 ED_xxx
 """
+ED_EVENTS_START = pygame.constants.USEREVENT + 1 + 4 * DIFF
 ED_events = dict()
+ED_events["ED_Start"] = 
